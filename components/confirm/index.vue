ed3f0b8ca8c03d8185777b062bcda4db69a12d69
ViavaCos <13811092694@163.com>
2021-05-30 19:31:20 +0800

    noting

---
+++ components/confirm/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-confirm">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'v-confirm'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ components/loading/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-loading">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'v-loading'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
